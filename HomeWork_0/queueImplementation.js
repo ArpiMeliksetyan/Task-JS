60c2e2d2581932976c0816924a9665470d4a2969
Arpi <a.meliqsetyan@inbox.ru>
2021-01-29 16:51:11 +0400

    stack&queue

--- HomeWork_0/queueImplementation.js
+++ HomeWork_0/queueImplementation.js
@@ -1,10 +1,38 @@
-
-const createQueue = () => {
-    const array = [];
-    const array1 = [];
-
+const createStack = () => {
+    let array = [];
     let size = 0;
 
+    return {
+        getSize: function () {
+            return size;
+        },
+
+        isEmpty: function () {
+            return size === 0;
+        },
+
+        push: function (value) {
+            size++;
+            array.push(value);
+        },
+
+        pop: function () {
+            if (this.isEmpty()) return;
+            size--;
+            return array.pop();
+        },
+        peek: function () {
+            if (this.isEmpty()) return;
+            return array[array.length - 1];
+        }
+    }
+}
+const createQueue = () => {
+
+    let stack1 = createStack();
+    let stack2 = createStack();
+
+    let size = 0;
 
     return {
         getSize: function () {
@@ -16,26 +44,25 @@ const createQueue = () => {
         },
 
         enqueue: function (value) {
-            size++;
-            array.push(value);
+            stack1.push(value);
         },
 
         dequeue: function () {
-            while (array.length > 0) {
-                let element = array.pop();
-                array1.push(element);
+            while (stack1.getSize() > 0) {
+                let element = stack1.pop();
+                stack2.push(element);
             }
             size--;
-            return array1.pop();
+            return stack2.pop();
 
         },
 
-        peek: function(){
-            while (array.length > 0) {
-                let element = array.pop();
-                array1.push(element);
+        peek: function () {
+            while (stack1.getSize() > 0) {
+                let element = stack1.pop();
+                stack2.push(element);
             }
-            return array1[array1.length-1];
+            return stack2.peek();
         }
 
     };
@@ -46,8 +73,11 @@ const queue = createQueue();
 queue.enqueue(1);
 queue.enqueue(2);
 queue.enqueue(3);
-console.log(queue.isEmpty())
+queue.enqueue(4);
+queue.enqueue(5);
+queue.enqueue(6);
 console.log(queue.dequeue());
-console.log(queue.peek())
+console.log(queue.peek());
 console.log(queue.dequeue());
-console.log(queue.isEmpty())
+console.log(queue.dequeue());
+console.log(queue.peek());
